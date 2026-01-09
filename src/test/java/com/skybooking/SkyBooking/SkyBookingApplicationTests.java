3a774d4ca09f5969835feed9bbe9ce3d70dbc9aa
LucasFagundez <lfagundezm01@iescastelar.com>
2026-01-09 09:07:04 +0100

    Inicio del proyecto

---
+++ src/test/java/com/skybooking/SkyBooking/SkyBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skybooking.SkyBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

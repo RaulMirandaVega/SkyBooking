58857a85efe3a5695327ff4bab54ef77f80b61a5
RaulMirandaVega <rmirandav02@iescastelar.com>
2026-01-12 14:07:54 +0100

    Cambios en Avion.java y creacion de Vuelo.java

--- src/main/java/com/skybooking/model/Avion.java
+++ src/main/java/com/skybooking/model/Avion.java
@@ -5,22 +5,22 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Data
-@AllArgsContructor
-@NoArgsContructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name="aviones")
 public class Avion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
-    private long id;
+    private Long id;
     @Column(unique = true, nullable = false)
     private String matricula;
-    @Column(nullable = true)
+    @Column(nullable = false)
     private String modelo;
     @Column(name = "capacidad_turista", nullable = false)
     private Integer capacidadTurista;
     @Column(name = "capacidad_business", nullable = false)
     private Integer capacidadBusiness;
-    @OneToMany(mappedBy = "avion", cascade = Cascade.ALL)
+    @OneToMany(mappedBy = "avion")
     private List<Vuelo> vuelos;
 }

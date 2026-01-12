03e0f904a52f54d34fcbe7785e4db80fbd54082e
RaulMirandaVega <rmirandav02@iescastelar.com>
2026-01-12 13:48:33 +0100

    Configuracion final Avion.java

--- src/main/java/com/skybooking/model/Avion.java
+++ src/main/java/com/skybooking/model/Avion.java
@@ -1,4 +1,8 @@
-import loombook.*;
+package com.skybooking.model;
+
+import lombok.*;
+import jakarta.persistence.*;
+import java.util.List;
 
 @Data
 @AllArgsContructor
@@ -9,12 +13,14 @@ public class Avion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private long id;
-    @Column
+    @Column(unique = true, nullable = false)
     private String matricula;
-    @Column
+    @Column(nullable = true)
     private String modelo;
-    @Column(name = "capacidad_turista")
-    private String capacidadTurista;
-    @Column(name = "capacidad_business")
-    private String capacidadBusiness;
+    @Column(name = "capacidad_turista", nullable = false)
+    private Integer capacidadTurista;
+    @Column(name = "capacidad_business", nullable = false)
+    private Integer capacidadBusiness;
+    @OneToMany(mappedBy = "avion", cascade = Cascade.ALL)
+    private List<Vuelo> vuelos;
 }

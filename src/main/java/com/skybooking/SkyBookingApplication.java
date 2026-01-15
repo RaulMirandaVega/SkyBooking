ac1541f8b53b98715f6f0a1885282811e416652f
LucasFagundez <lfagundezm01@iescastelar.com>
2026-01-15 08:27:49 +0100

    Reeserva controler

--- src/main/java/com/skybooking/SkyBookingApplication.java
+++ src/main/java/com/skybooking/SkyBookingApplication.java
@@ -1,4 +1,4 @@
-package com.skybooking.SkyBooking;
+package com.skybooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

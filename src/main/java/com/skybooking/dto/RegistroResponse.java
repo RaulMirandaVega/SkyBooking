4271eb899ca11da0bdce387d0d8a47953d3160fe
RaulMirandaVega <rmirandav02@iescastelar.com>
2026-01-29 08:51:54 +0100

    Crear DTOs de Autenticación

---
+++ src/main/java/com/skybooking/dto/RegistroResponse.java
@@ -0,0 +1,27 @@
+package com.skybooking.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistroResponse {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+    private String nombre;
+    private String apellidos;
+    private Set<String> roles;
+}

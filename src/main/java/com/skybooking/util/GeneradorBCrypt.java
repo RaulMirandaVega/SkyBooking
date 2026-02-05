f7b98529c17e69d50173f3e7da58a2f5b93a6f07
RaulMirandaVega <rmirandav02@iescastelar.com>
2026-02-05 08:54:37 +0100

    Revert "BCrypt"
    This reverts commit 8bbf823803a84aa0c14bc705d253d00f61e49ccb.

--- src/main/java/com/skybooking/util/GeneradorBCrypt.java
+++ src/main/java/com/skybooking/util/GeneradorBCrypt.java
@@ -5,13 +5,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class GeneradorBCrypt {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String plain = "123456";
-        String hash = encoder.encode(plain);
-        System.out.println("Generated hash: " + hash);
-        System.out.println("Check matches: " + encoder.matches(plain, hash));
-        System.out.println("-- SQL to update users:");
-        System.out.println("UPDATE usuarios SET password = '" + hash + "' WHERE username = 'admin';");
-        System.out.println("UPDATE usuarios SET password = '" + hash + "' WHERE username = 'empleado';");
-        System.out.println("UPDATE usuarios SET password = '" + hash + "' WHERE username = 'usuario';");
+        String password = encoder.encode("123456");
+        String hash = encoder.encode(password);
+        System.out.println("Hash: " + hash);
     }
 }
